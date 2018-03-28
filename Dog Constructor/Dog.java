2a5388cb15d7fae75abc4c48b790348db9a82c8d
JShin39 <36411026+JShin39@users.noreply.github.com>
2018-03-28 09:28:34 -0400

    Add files via upload

---
+++ Dog Constructor/Dog.java
@@ -0,0 +1,28 @@
+public class Dog {
+  String name;
+  String breed;
+  String furColor;
+  int birthYear;
+  
+  // Create constructor here!
+  public Dog(String name, String breed, String furColor, int birthYear) {
+    this.name = name;
+    this.breed = breed;
+    this.furColor = furColor;
+    this.birthYear = birthYear;
+  }
+  
+  public void sayHello() {
+    System.out.println("Woof");
+  }
+  
+  public void sayName() {
+    System.out.println("Woof");
+  }
+  
+  public int getAge() {
+    int humanYears = 2018 - birthYear;
+    int dogYears = humanYears * 7;   // not the most accurate
+    return dogYears;
+  }
+}
